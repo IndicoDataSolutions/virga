45f751f10879f4d3e9c748568ae65798566727af
thearchitector <elias.gabriel@indico.io>
2021-03-03 11:13:11 -0500

    UPDATE: alter template and patch for clarity

--- virga/cli/templates/boilerplate/boilerplate/app.py
+++ virga/cli/templates/boilerplate/boilerplate/app.py
@@ -1,8 +1,10 @@
 from functools import lru_cache
-
 from fastapi import FastAPI, Depends
-from .settings import Settings
 
+# from virga.types import User
+# from virga.requests.noct import get_current_user
+
+from .settings import Settings
 
 app = FastAPI()
 
@@ -10,6 +12,12 @@ app = FastAPI()
 @lru_cache()
 def get_settings():
     return Settings()
+
+
+# # Makes use of Noct middleware to fetch the current authenticated user
+# @app.get("/user_info")
+# async def get_user(current_user: User = Depends(get_current_user)):
+#     return {"message": f"Hello, {current_user.name}!"}
 
 
 @app.get("/")
