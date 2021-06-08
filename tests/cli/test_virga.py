6022e7697fd225b3fad68e79f52e52593cbde6ae
Elias Gabriel <elias.gabriel@indico.io>
2021-06-08 14:19:21 -0700

    FIX: tests

--- tests/cli/test_virga.py
+++ tests/cli/test_virga.py
@@ -6,48 +6,18 @@ from itertools import combinations
 from virga._cli.application import virga
 
 
-# non-empty app path
-def test_virga_new_bad_non_empty():
+def test_virga_new_exists():
     runner = CliRunner()
 
     with runner.isolated_filesystem():
-        os.mkdir(f"full-directory")
+        os.mkdir("full-directory")
         open("full-directory/file.txt", "a").close()
         result = runner.invoke(virga, ["new", "full-directory"])
         assert result.exit_code == 2
-        assert result.output.find("already exists and is not empty") > -1
+        assert result.output.find("already exists") > -1
 
 
-# existing file for app path
-def test_virga_new_bad_file():
-    runner = CliRunner()
-
-    with runner.isolated_filesystem():
-        open("file.txt", "a").close()
-        result = runner.invoke(virga, ["new", "file.txt"])
-        assert result.exit_code == 2
-        assert result.output.find("non-existent file or directory") > -1
-
-
-# nested non-existent directories
-def test_virga_new_good_nested():
-    runner = CliRunner()
-
-    with runner.isolated_filesystem():
-        result = runner.invoke(virga, ["new", "foo/bar/new-project"])
-        assert result.exit_code == 0
-        assert result.output.find("Virga application generation complete!") > -1
-
-        assert os.path.isdir("foo")
-        assert os.path.isdir("foo/bar")
-        assert os.path.isdir("foo/bar/new-project")
-        assert os.path.isfile("foo/bar/new-project/api/pyproject.toml")
-        assert os.path.isfile("foo/bar/new-project/api/poetry.lock")
-        assert os.path.isfile("foo/bar/new-project/api/Dockerfile")
-
-
-# single non-existent directory
-def test_virga_new_good_direct():
+def test_virga_new():
     runner = CliRunner()
 
     with runner.isolated_filesystem():
@@ -67,7 +37,7 @@ cli_args = ["--auth", "--graphql", "--webui", "--database"]
 @pytest.mark.parametrize(
     "opts",
     [
-        pytest.param(args, id=",".join(args).replace("--", ""))
+        pytest.param(args, id=",".join(args))
         for n in range(len(cli_args))
         for args in combinations(cli_args, n + 1)
     ],
