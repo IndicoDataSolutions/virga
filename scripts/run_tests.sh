05a81905db4299a0724a4294caa30c8605eb418b
thearchitector <elias.gabriel@indicodata.ai>
2023-02-21 11:09:32 -0500

    ci: fix pipeline

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-until (curl -sf "http://${POSTGRES_HOST}:5000/api/ping"); do
+until (curl -sf "${NOCT_HOST:-http://noct:5000}/api/ping"); do
     ((count++))
 
-    if [ ${count} -gt 500 ]; then
+    if [ ${count} -gt 200 ]; then
         echo "Noct unavailable"
         exit 1
     fi
 
     echo "Waiting for noct"
-    sleep 0.5
+    sleep 1
 done
 
 pytest -vv tests
